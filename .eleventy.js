8a7a8f8589ed72c4295bc33627247341c55b9b21
John Williams <john@thudfactor.com>
2022-12-04 15:06:54 -0500

    Oh hi mark

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventySass = require("eleventy-sass");
 const pluginRev = require("eleventy-plugin-rev");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPlugin(pluginRev);
   eleventyConfig.addPlugin(eleventySass, {
     rev: true

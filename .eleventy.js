efee326faf0b4ac5e3f44eff55e5e0ac538320cf
John Williams <john@thudfactor.com>
2022-12-10 17:18:11 -0500

    More bloggy goodness

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,40 @@
 const eleventySass = require("eleventy-sass");
 const pluginRev = require("eleventy-plugin-rev");
+const path = require("path");
+const Image = require("@11ty/eleventy-img");
+
+const dir = {
+  input: "src",
+  output: "dist",
+  layouts: "_layouts"
+};
+
+function pageInfo() {
+  return '<h1>foo</h1>';
+};
+
+async function imageShortcode(page, src, alt, width = "1024") {
+  const { inputPath } = page;
+  const filePath = path.dirname(inputPath);
+
+  const computedSrc = `${filePath}/${src}`;
+
+  const metadata = await Image(computedSrc, {
+    formats: "webp",
+    widths: [width],
+    outputDir: "./dist/images",
+    urlPath: "/images/"
+  });
+
+  const imageAttributes = {
+    alt,
+    sizes: '',
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
@@ -7,11 +42,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass, {
     rev: true
   });
-  return {
-    dir: {
-      input: "src",
-      output: "dist",
-      layouts: "_layouts"
-    }
-  }
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addShortcode("pageInfo", pageInfo);
+  return { dir };
 };

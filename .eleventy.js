06acd8c73e1d04607dd9dc30ef9656f97615ae74
John Williams <john@thudfactor.com>
2023-07-29 19:10:28 -0400

    Revise about

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ const { extractExcerpt, alertCallout } = require("./config/shortcodes");
 const { image, coverImage, facebookImagePath } = require("./config/shortcodes/images");
 const { filterByCategory, nicedate } = require("./config/filters");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+//const markdownItFootnote = require("markdown-it-footnote");
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(pluginRss);

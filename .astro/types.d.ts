69ca0f583eaf67190ff33af600a0a173847825ce
John Williams <john@thudfactor.com>
2024-05-29 07:37:47 -0400

    Adventures in vim part 1

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -306,6 +306,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024/05/vim-1/index.mdx": {
+	id: "2024/05/vim-1/index.mdx";
+  slug: "2024/05/vim-1";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 
 	};

4d3409ff8c99f583f9c8b3b751fb192c0fc2ee19
John Williams <john@thudfactor.com>
2024-05-30 07:15:18 -0400

    Update cover page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,40 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-	type: 'content',
+	type: "content",
 	// Type-check frontmatter using a schema
-	schema: ({image}) => z.object({
-		title: z.string(),
-		subtitle: z.string().optional(),
-		description: z.string(),
-		// Transform string to Date object
-		date: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		cover: z.object({
-      src: image(),
-      alt: z.string(),
-      description: z.string().optional(),
-			attribution: z.object({
-				name: z.string(),
-				link: z.string().url().optional(),
-				license: z.object({
-					name: z.string(),
-					link: z.string().url().optional(),
-				}).optional()
-			}).optional()
-    }).optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			subtitle: z.string().optional(),
+			description: z.string(),
+			// Transform string to Date object
+			date: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			cover: z
+				.object({
+					src: image(),
+					alt: z.string(),
+					description: z.string().optional(),
+					attribution: z
+						.object({
+							name: z.string(),
+							link: z.string().url().optional(),
+							license: z
+								.object({
+									name: z.string(),
+									link: z.string().url().optional(),
+								})
+								.optional(),
+						})
+						.optional(),
+				})
+				.optional(),
+		}),
 });
 
 const pages = defineCollection({
-	type: 'content',
+	type: "content",
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),

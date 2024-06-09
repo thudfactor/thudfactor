f7f118c135eee41a17a72274793a08a33a7ae61e
John Williams <john@thudfactor.com>
2024-06-09 15:18:32 -0400

    Update layout a bit

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -313,6 +313,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024/06/vim-2/index.mdx": {
+	id: "2024/06/vim-2/index.mdx";
+  slug: "2024/06/vim-2";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 
 	};

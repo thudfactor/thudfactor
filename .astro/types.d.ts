7be3464c67814f1adc06a647281e8c27f823731d
John Williams <john@thudfactor.com>
2024-07-02 17:22:16 -0400

    Domain shift

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -334,6 +334,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024/07/domain-shift/index.mdx": {
+	id: "2024/07/domain-shift/index.mdx";
+  slug: "2024/07/domain-shift";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 
 	};

5b1da6f3c23c0d449da108f8d818aed11033980e
John Williams <john@thudfactor.com>
2025-02-01 15:41:35 -0500

    gettin serious about that redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from '@astrojs/sitemap';
 
@@ -7,4 +8,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://www.thudfactor.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	}
 });

b5cfa041d3c4b9418a903ea9ac8f4308301066fd
John Williams <john@thudfactor.com>
2025-04-21 07:20:00 -0400

    Fix RSS date and permalink issue, support drafts

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,17 +1,19 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
+	const posts = await getCollection("posts", (p) => p.data.draft !== true);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			pubDate: post.data.date,
-			link: `/posts/${post.slug}/`,
-			...post.data,
-		})),
+		items: posts
+			.map((post) => ({
+				pubDate: post.data.date,
+				link: `/posts/${post.id}/`,
+				...post.data,
+			}))
+			.sort((pa, pb) => pb.pubDate.getTime() - pa.pubDate.getTime()),
 	});
 }

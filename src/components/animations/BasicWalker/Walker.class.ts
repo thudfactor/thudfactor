afcd8251b61d560c1dc5c5fe44dcd48cb381a3f0
John Williams <john@thudfactor.com>
2025-06-15 09:13:39 -0400

    Changing approach to js, getting client js working, starting to pull in other examples

---
+++ src/components/animations/BasicWalker/Walker.class.ts
@@ -0,0 +1,35 @@
+import type p5 from "p5";
+
+class Walker {
+	x: number;
+	y: number;
+	p: p5;
+
+	constructor(p: p5) {
+		this.x = p.width / 2;
+		this.y = p.height / 2;
+		this.p = p;
+	}
+
+	show() {
+		console.log("step");
+		this.p.stroke(255);
+		this.p.strokeWeight(10);
+		this.p.point(this.x, this.y);
+	}
+
+	step() {
+		const choice = this.p.floor(this.p.random(4));
+		if (choice === 0) {
+			this.x++;
+		} else if (choice === 1) {
+			this.x--;
+		} else if (choice === 2) {
+			this.y++;
+		} else if (choice === 3) {
+			this.y--;
+		}
+	}
+}
+
+export default Walker;

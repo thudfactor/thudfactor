ccca9dfcf0e989ef2ef6ee0a24463dc99c923df3
John Williams <john@thudfactor.com>
2025-07-04 08:32:16 -0400

    Attempting image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { remarkDefinitionList, defListHastHandlers } from "remark-definition-list";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 //import db from "@astrojs/db";
 
@@ -10,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 	site: "https://www.thudfactor.com",
 	integrations: [mdx(), sitemap()],
+	adapter: vercel({ imageService: true }),
 
 	vite: {
 		plugins: [tailwindcss()],
